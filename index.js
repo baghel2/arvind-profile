8440d7ae69b485791b6a67f4ef6a6eeccd6becdf
arv <arvindsinghbaghel700@gmail.com>
2018-07-26 17:01:17 +0530

    ind
    ind

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const images = ['fox1','fox2','fox3','fox4'];
-const imgElem = document.querySelector('img');
-
-function randomValueFromArray(array) {
-  let randomNo =  Math.floor(Math.random() * array.length);
-  return array[randomNo];
-}
-
-setInterval(function() {
-  let randomChoice = randomValueFromArray(images);
-  imgElem.src = 'images/' + randomChoice + '.jpg';
-}, 2000)
-
 // Register service worker to control making site work offline
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('sw.js')
-           .then(function() { console.log('Service Worker Registered'); });
+    .register('sw.js')
+    .then(function() { console.log('Service Worker Registered'); })  
+    .catch(function(error) {console.log('Service worker registration failed, error:', error);  });
 }
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'block';
+});

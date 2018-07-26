ec517d680340fb23abe59ab62512393b30ed823b
arv <arvindsinghbaghel700@gmail.com>
2018-07-26 17:06:31 +0530

    sw
    sw

--- sw.js
+++ sw.js
@@ -1,19 +1,18 @@
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('video-store').then(function(cache) {
-     return cache.addAll([
-       '/',
-       'index.html',
-       'index.js',
-       'style.css',
-       'images/fox1.jpg',
-       'images/fox2.jpg',
-       'images/fox3.jpg',
-       'images/fox4.jpg'
-     ]);
-   })
- );
+var VERSION = '2';
+this.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(VERSION).then(cache => {
+    return cache.addAll([
+      '/',
+      'index.html',
+      'sw.js',
+      'style.css',
+     'Icon.png',
+     'fox-icon.png',
+     'icon-512.png'
+    ]);
+  }))
 });
+
 
 self.addEventListener('fetch', function(e) {
   console.log(e.request.url);
@@ -23,3 +22,24 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+/*
+function fetch(url) {
+  return fetch(url)
+    .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(VERSION)
+      .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+    .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
+  });
+}
+*/
